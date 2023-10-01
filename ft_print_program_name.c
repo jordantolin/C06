14b1afd5a747c9910efeab00d601877276b8fdad
jordantolin <138111277+jordantolin@users.noreply.github.com>
2023-10-02 00:27:08 +0200

    Add files via upload

---
+++ ft_print_program_name.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+int     main(int    argc, char  **argv)
+{
+    (void)argc;
+
+    int i;
+
+    i = 0;
+    while (argv [0][i])
+    {
+        ft_putchar(argv[0][i]);
+        i++;
+    }
+    ft_putchar('\n');
+    return (0);
+}
